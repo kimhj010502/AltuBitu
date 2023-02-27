40ac3848c4a10028a8f13acf992ee1f82c4dcc16
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-02-27 23:43:48 +0900

    Add files via upload

--- Lecture02/Assignment/BOJ_4949.cpp
+++ Lecture02/Assignment/BOJ_4949.cpp
@@ -1,1 +1,65 @@
-.
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string check(string& input, stack<char>& s) {
+	
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == '(' || input[i] == '[') {
+			s.push(input[i]);
+		}
+		else if (input[i] == ')') {
+			if (s.empty()) {
+				return "no";
+			}
+			else {
+				if (s.top() == '(') {
+					s.pop();
+				}
+				else {
+					return "no";
+				}
+			}
+		}
+		else if (input[i] == ']') {
+			if (s.empty()) {
+				return "no";
+			}
+			else {
+				if (s.top() == '[') {
+					s.pop();
+				}
+				else {
+					return "no";
+				}
+			}
+		}
+	}
+
+	if (s.empty()) {
+		return "yes";
+	}
+	else {
+		return "no";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string input;
+	
+	while (true) {
+		getline(cin, input);
+		stack<char> s;
+		if (input == ".") {
+			break;
+		}
+		cout << check(input, s) << "\n";
+	}
+	
+	return 0;
+}
