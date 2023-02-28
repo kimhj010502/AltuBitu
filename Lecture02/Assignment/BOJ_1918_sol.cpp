e43793daacf54d162c43d8c887d2c3dd159259ad
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-02-28 21:30:40 +0900

    Add files via upload

---
+++ Lecture02/Assignment/BOJ_1918_sol.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int priority(char ch) { //연산자 우선순위 반환
+	switch (ch) {
+		case '*': case '/': return 2;
+		case '+': case '-': return 1;
+		case '(': return 0;
+	}
+}
+
+string postfix(string input) { //중위 표기식 → 후위 표기식
+	string result = "";
+	stack<char> op;
+
+	for (int i = 0; i < input.length(); i++) {
+		/*연산자가 나오는 경우, 우선순위 비교하기
+		피연산자가 나오는 경우, 곧바로 결과에 추가하기*/
+
+		char curr = input[i];
+		switch (curr) {
+			case '(': //여는 괄호: 곧바로 스택에 추가
+				op.push(curr);
+				break;
+			case ')': //닫는 괄호: 여는 괄호가 나오기 전까지 스택의 연산자 결과에 추가하기
+				while (!op.empty() && op.top() != '(') {
+					result += op.top();
+					op.pop();
+				}
+				op.pop(); //여는 괄호 삭제
+				break;
+
+			case '+': case '-': case '*': case'/': //연산자 우선순위 비교
+				//curr 연산자를 넣으려면, curr 연산자보다 높은 우선순위를 가지는 연산자를 pop해야 함
+				while (!op.empty() && priority(curr) <= priority(op.top())) {
+					result += op.top();
+					op.pop();
+				}
+				op.push(curr); //현재 연산자 넣기
+				break;
+
+			default: //피연산자
+				result += curr;
+		}
+	}
+
+	while (!op.empty()) {
+		result += op.top();
+		op.pop();
+	}
+
+	return result;
+}
+
+int main() {
+	//입력
+	string input; //중위 표기식 입력값
+	cin >> input;
+
+	//연산 & 출력
+	cout << postfix(input); //후위 표기식 출력값
+
+	return 0;
+}
---
+++ Lecture02/Assignment/PRO_스택큐_기능개발.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+queue<int> calculateTime(vector<int> progresses, vector<int> speeds) {
+    queue<int> q;
+    
+    for (int i = 0; i < progresses.size(); i++) {
+        int time = ceil((100 - progresses[i]) / floor(speeds[i]));
+        q.push(time);
+    }
+
+    return q;
+}
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+
+    queue<int> q = calculateTime(progresses, speeds);
+
+    while (!q.empty()) {
+        int day = q.front();
+        int count = 1;
+        q.pop();
+
+        while (!q.empty() && day >= q.front()) {
+            count++;
+            q.pop();
+        }
+
+        answer.push_back(count);
+    }
+
+    return answer;
+}
+
+int main() {
+
+    //입력
+    int t;
+    cin >> t;
+
+    vector<int> progresses(t);
+    vector<int> speeds(t);
+
+    for (int i = 0; i < t; i++) {
+        cin >> progresses[i];
+    }
+
+    for (int i = 0; i < t; i++) {
+        cin >> speeds[i];
+    }
+
+    //연산
+    vector<int> result = solution(progresses, speeds);
+
+    //출력
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
