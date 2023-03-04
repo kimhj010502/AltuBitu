07313afbd42627886cce53173c409ffbeb7d33c5
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-03-04 19:24:31 +0900

    Add files via upload

--- Lecture02/AddProblem/BOJ_2164.cpp
+++ Lecture02/AddProblem/BOJ_2164.cpp
@@ -1,1 +1,28 @@
-d
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	//입력
+	int n;
+	cin >> n;
+
+	//연산
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	while (q.size() > 1) {
+		q.pop(); //제일 위의 카드 버리기
+		int card = q.front(); //그 다음 제일 위에 있는 카드 밑으로 옮기기
+		q.push(card);
+		q.pop();
+	}
+
+	//출력
+	cout << q.front();
+
+	return 0;
+}
