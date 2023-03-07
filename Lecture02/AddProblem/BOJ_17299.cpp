211dc9e1c748e96d34ad815053688e36ebbd0b71
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-03-08 02:16:07 +0900

    Add files via upload

--- Lecture02/AddProblem/BOJ_17299.cpp
+++ Lecture02/AddProblem/BOJ_17299.cpp
@@ -1,63 +1,53 @@
 ﻿#include <iostream>
 #include <vector>
-#include <map>
 #include <stack>
 
 using namespace std;
 
-map<int, int> calF(vector<int>& a) { //F를 구하는 함수
-	map<int, int> m;
+const int MAX = 1000000;
+
+//F를 계산하는 함수
+stack<pair<int, int>> calF(vector<int>& a) {
+	vector<int> m(MAX + 1); //F(A)
 	for (int i = 0; i < a.size(); i++) {
 		m[a[i]]++;
 	}
-	return m;
+	stack<pair<int, int>> f; //A, F값을 역순으로 담은 스택
+	for (int i = 0; i < a.size(); i++) {
+		f.push({ a[i], m[a[i]] });
+	}
+	return f;
 }
 
-vector<int> calNGF(vector<int>& a, const int& n) {
-	map<int, int> m = calF(a); //<A, F(A)>
-	vector<int> ngf(n);
-
-	vector<int> f(n); //F(A1), ... , F(An)로 이루어진 배열
-	for (int i = 0; i < n; i++) {
-		f[i] = m[a[i]];
-	}
-
-	stack<int> s; //F가 더 큰 A를 담은 스택
-	ngf[n - 1] = -1; //가장 마지막 값은 항상 -1
-	s.push(a[n - 1]); //처음에는 F값이 가장 큰 A가 마지막 A임
-
-	int index = n - 2; //index를 마지막에서 두번째로 지정해주고 하나씩 줄여나가면서 NGF 구함
-
-	while (!s.empty() && f[index] > m[s.top()]) {
-		s.pop();
-	}
-
-
-
-	while (!s.empty()) { //스택에 값이 있을 동안 반복
-		while (f[index] > m[s.top()]) {
+//NGF 계산하는 함수
+stack<int> calNGF(vector<int>& a) {
+	stack<pair<int, int>> f = calF(a); //<A, F(A)>
+	stack<pair<int, int>> s;
+	stack<int> ngf;
+	while (!f.empty()) { //모든 <A, F>에 대해서 연산을 끝낼 때까지 반복
+		//s가 비어있으면 A의 오른쪽에 있으면서 F가 큰 수가 없는 경우
+		if (s.empty()) { 
+			//-1을 넣고 f에 있는 수를 s로 옮김
+			ngf.push(-1); 
+			s.push(f.top()); 
+			f.pop();
+			continue;
+		}
+		
+		//s에 있는 A의 F가 f에 있는 A의 F보다 큰 경우
+		//s.top()은 A의 오른쪽에 있으면서 F가 큰 수 중에서 가장 왼쪽에 있는 수
+		if (s.top().second > f.top().second) {
+			//그 수의 A를 넣고 f에 있는 수를 s로 옮김
+			ngf.push(s.top().first); 
+			s.push(f.top());
+			f.pop();
+		}
+		//s에 있는 A의 F가 f에 있는 A의 F보다 작거나 같은 경우
+		else {
+			//F가 더 큰 수가 나올 때까지 s의 원소 제거
 			s.pop();
 		}
-
-
-
-		if (f[index] < m[s.top()]) { //현재 F값보다 스택에 있는 A의 F값이 큰 경우
-			ngf[index] = s.top(); //현재 인덱스에 해당하는 NGF값은 s.top()임
-			s.push(a[index--]); //앞의 F보다 클 수 있으므로 스택에 저장
-		}
-		else { //현재 F값보다 스택에 있는 A의 F값이 작거나 같은 경우
-			s.pop(); //이전에 스택에 저장해둔 값은 이제 쓰일 일이 없으므로 삭제
-			if (!s.empty()) { //스택이 비어있지 않다면
-				ngf[index--] = s.top(); //이전의 값 복사
-			}
-			s.push(a[index--]); //같을때?
-		}
 	}
-
-	while (index >= 0) { //스택에 값이 없어지면 나머지는 모두 -1로 채워짐
-		ngf[index--] = -1;
-	}
-
 	return ngf;
 }
 
@@ -74,11 +64,12 @@ int main() {
 	}
 
 	//연산
-	vector<int> ngf = calNGF(a, n);
+	stack<int> ngf = calNGF(a);
 
 	//출력
-	for (int i = 0; i < n; i++) {
-		cout << ngf[i] << " ";
+	while (!ngf.empty()) {
+		cout << ngf.top() << " ";
+		ngf.pop();
 	}
 
 	return 0;
