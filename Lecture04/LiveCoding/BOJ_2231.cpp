6753ce96edc0627f2f02138c8f303758dc549de0
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-03-12 00:16:54 +0900

    Add files via upload

---
+++ Lecture04/LiveCoding/BOJ_2231.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string solution(int n) {
+	for (int i = 1; i < n; i++) {
+		string num = to_string(i);
+		int sum_num = i;
+		for (int j = 0; j < num.size(); j++) {
+			sum_num += num[j] - '0';
+		}
+		if (sum_num == n) {
+			return num;
+		}
+	}
+	return "0";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	//연산 & 출력
+	cout << solution(n);
+
+	return 0;
+}
