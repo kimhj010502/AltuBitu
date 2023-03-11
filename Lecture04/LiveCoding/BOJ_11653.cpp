9e1aa8609e956c62d92a07108354aa6ab0c0960c
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-03-11 23:57:07 +0900

    Add files via upload

---
+++ Lecture04/LiveCoding/BOJ_11653.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> isPrime(int n) {
+	vector<int> is_prime(n + 1, true);
+	
+	for (int i = 2; i * i <= n; i++) {
+		if (!is_prime[i]) { //소수가 아니면 넘어감
+			continue;
+		}
+		for (int j = i * 2; j <= n; j += i) { //소수의 배수 다 지우기
+			if (!is_prime[j]) { //이미 지워졌으면 넘어감
+				continue;
+			}
+			is_prime[j] = false; //소수가 아님
+		}
+	}
+	return is_prime;
+}
+
+void factorization(int n) {
+	vector<int> is_prime = isPrime(n);
+
+	for (int i = 2; i <= n; i++) {
+		if (n == 1) {
+			break;
+		}
+		if (!is_prime[i]) {
+			continue;
+		}
+		while (n % i == 0) {
+			cout << i << "\n";
+			n /= i;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	//연산 & 출력
+	factorization(n);
+
+	return 0;
+}
--- Lecture04/LiveCoding/BOJ_2309.cpp
+++ Lecture04/LiveCoding/BOJ_2309.cpp
@@ -1,1 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+vector<int> solution(vector<int>& height) {
+	vector<int> result;
+	sort(height.begin(), height.end());
+
+	int sum_height = 0;
+	for (int i = 0; i < 9; i++) {
+		sum_height += height[i];
+	}
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = i; j < 9; j++) {
+			if (sum_height - height[i] - height[j] == 100) {
+				result.push_back(i); result.push_back(j);
+				return result;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	vector<int> height(9);
+	for (int i = 0; i < 9; i++) {
+		cin >> height[i];
+	}
+	
+	//연산
+	vector<int> result = solution(height);
+
+	//출력
+	for (int i = 0; i < 9; i++) {
+		if (find(result.begin(), result.end(), i) == result.end()) {
+			cout << height[i] << "\n";
+		}
+	}
+
+	return 0;
+}
