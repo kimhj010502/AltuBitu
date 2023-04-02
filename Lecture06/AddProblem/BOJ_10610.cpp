246464196ae25ec91bf64cc28d68b167bff78cda
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-04-02 22:17:43 +0900

    Add files via upload

--- Lecture06/AddProblem/BOJ_10610.cpp
+++ Lecture06/AddProblem/BOJ_10610.cpp
@@ -1,1 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+//30의 배수가 될 조건
+//1. 3의 배수 (각 자리수의 합이 3의 배수)
+//2. 일의 자리가 0인 수
+string solution(string n) {
+	sort(n.rbegin(), n.rend()); //내림차순으로 정렬
+
+	int sum_num = 0;
+	for (int i = 0; i < n.size(); i++) {
+		sum_num += n[i] - '0';
+	}
+	
+	//3의 배수가 아니거나, 10의 배수가 아니면 30의 배수가 존재하지 않으므로 -1 출력
+	if (sum_num % 3 != 0 || n[n.size() - 1] != '0') {
+		return "-1";
+	}
+	
+	return n;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	string n;
+	cin >> n;
+
+	//연산 & 출력
+	cout << solution(n);
+
+	return 0;
+}
---
+++ Lecture06/AddProblem/BOJ_16206.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//케이크의 길이가 10의 배수인 경우 자른 횟수에 비해 더 많은 케이크가 나옴
+//둘 다 10의 배수인 경우 케이크의 길이가 짧은 것이 더 많이 나올 가능성이 있음 (예시: 30 vs 20)
+//10의 배수 먼저, 길이가 짧은 케이크가 먼저 처리되도록 sort
+bool cmp(int a, int b) {
+	if (a % 10 == 0 && b % 10 == 0) {
+		return a < b;
+	}
+	else if (a % 10 == 0 && b % 10 != 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int countCake(vector<int>& a, int m) {
+	int cut_cnt = 0;
+	int total_cake = 0;
+	int cake_cnt;
+	bool multi_ten = false; //해당 케이크가 10의 배수인지 확인
+
+	//10의 배수가 앞에 오도록 정렬
+	sort(a.begin(), a.end(), cmp);
+
+	for (int i = 0; i < a.size(); i++) {
+		//cout << a[i] << ": ";
+		//해당 케이크를 잘랐을 때 생기는 길이가 10인 케이크의 개수
+		cake_cnt = a[i] / 10;
+		//전체 케이크 개수 업데이트
+		total_cake += cake_cnt;
+
+		//케이크의 길이를 10으로 나눈 나머지가 0이 아닌 경우
+		if (a[i] % 10) {
+			multi_ten = false;
+			cut_cnt += cake_cnt;
+		}
+		//나머지가 0인 경우
+		else {
+			multi_ten = true;
+			cut_cnt += cake_cnt - 1;
+		}
+		
+		//cout << "케이크 개수: " << cake_cnt << ", 전체 케이크 개수: " << total_cake << ", 자른 횟수: " << cut_cnt << "\n";
+
+		//자른 횟수가 m인 경우 바로 총 케이크의 개수 반환
+		if (cut_cnt == m) {
+			return total_cake;
+		}
+		else if (cut_cnt > m) {
+			//방금 앞에 잘랐던 케이크가 10의 배수인 경우
+			if (multi_ten) {
+				return total_cake - (cut_cnt - m) - 1;
+			}
+			//10의 배수가 아닌 경우
+			else {
+				return total_cake - (cut_cnt - m);
+			}
+		}
+		
+	}
+
+	//자를만큼 잘랐는데 m보다 작은 경우에도 total_cake 반환
+	return total_cake;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	//입력
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	//연산 & 출력
+	cout << countCake(a, m);
+
+	return 0;
+}
