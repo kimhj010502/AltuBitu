15fe34cae778d05fd47ecbeafcfda39b41b8286f
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-04-13 00:42:32 +0900

    Add files via upload

--- Lecture07/Assignment/BOJ_1325_sol.cpp
+++ Lecture07/Assignment/BOJ_1325_sol.cpp
@@ -4,15 +4,32 @@
 
 using namespace std;
 
+vector<bool> dfs_hacked;
+
+//dfs: 시간 초과
+int dfs(int n, int c, vector<vector<int>>& trust) {
+	int cnt = 1; //해당 컴퓨터가 해킹할 수 있는 컴퓨터 수
+	dfs_hacked.assign(n + 1, false);
+	dfs_hacked[c] = true;
+
+	for (int i = 0; i < trust[c].size(); i++) {
+		int next_c = trust[c][i];
+		if (!dfs_hacked[next_c]) {
+			cnt += dfs(n, next_c, trust);
+		}
+	}
+	return cnt;
+}
+
 //start 컴퓨터를 시작으로 해킹한 컴퓨터의 개수 출력
-int hacking(int n, int start, vector<vector<int>>& trust) {
+int bfs(int n, int start, vector<vector<int>>& trust) {
 	int result = 1; //해킹한 컴퓨터 개수
-	vector<int> hacked(n + 1, false);
+	vector<bool> bfs_hacked(n + 1, false);
 
 	//처음 시작 컴퓨터 초기화
 	queue<int> q;
 	q.push(start);
-	hacked[start] = true;
+	bfs_hacked[start] = true;
 
 	while (!q.empty()) {
 		int c = q.front();
@@ -21,9 +38,9 @@ int hacking(int n, int start, vector<vector<int>>& trust) {
 		//연결된 컴퓨터 다 해킹
 		for (int i = 0; i < trust[c].size(); i++) {
 			//아직 해킹되지 않은 컴퓨터라면 해킹
-			if (!hacked[trust[c][i]]) {
+			if (!bfs_hacked[trust[c][i]]) {
 				q.push(trust[c][i]);
-				hacked[trust[c][i]] = true;
+				bfs_hacked[trust[c][i]] = true;
 				result++;
 			}
 		}
@@ -37,7 +54,7 @@ vector<int> maxHacking(int n, vector<vector<int>>& trust) {
 	int max_cnt = 0;
 	for (int i = 1; i < n + 1; i++) {
 		//해킹 가능한 컴퓨터 개수 저장
-		int hacking_cnt = hacking(n, i, trust);
+		int hacking_cnt = bfs(n, i, trust);
 
 		//가장 많은 컴퓨터를 해킹할 수 있는 컴퓨터의 번호 저장
 		if (hacking_cnt > max_cnt) {
