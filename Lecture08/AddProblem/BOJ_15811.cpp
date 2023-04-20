37d2eced5fa2ae6a58b4a2b51331f29d8a478bf4
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-04-20 16:38:24 +0900

    Add files via upload

---
+++ Lecture08/AddProblem/BOJ_15811.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> word(3);
+vector<int> alphabet_num(26, -1); //알파벳에 대응되는 숫자 저장
+vector<bool> num(10, false); //0~9의 숫자가 사용되었는지의 여부 저장
+vector<bool> is_exist(26, false); 
+vector<int> alphabet;
+int alphabet_cnt; //사용된 알파벳의 개수
+bool is_possible = false; //계산식에 해답이 존재하는지의 여부
+
+//사용된 알파벳의 개수 반환
+int countAlphabet() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < word[i].size(); j++) {
+			is_exist[word[i][j] - 65] = true;
+		}
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < 26; i++) {
+		if (is_exist[i]) {
+			cnt++;
+			alphabet.push_back(i);
+		}
+	}
+	return cnt;
+}
+
+//계산식이 성립하는지의 여부 반환
+bool check() {
+	vector<int> word_num(3);
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < word[i].size(); j++) {
+			word_num[i] = word_num[i] * 10 + alphabet_num[word[i][j] - 65];
+		}
+	}
+
+	if (word_num[0] + word_num[1] == word_num[2]) {
+		return true;
+	}
+	return false;
+}
+
+void backtracking(int cnt) {
+	//이미 발견했으면 종료
+	if (is_possible) {
+		return;
+	}
+
+	//종료 조건: 모든 알파벳이 대응된 경우
+	if (cnt == alphabet_cnt) {
+		//식이 성립한다면
+		if (check()) {
+			is_possible = true;
+		}
+		return;
+	}
+
+	//10개의 숫자 대응
+	for (int j = 0; j < 10; j++) {
+		//이미 대응된 숫자의 경우 넘어감
+		if (num[j]) {
+			continue;
+		}
+
+		//사용
+		alphabet_num[alphabet[cnt]] = j;
+		num[j] = true;
+
+		//다음 문자 대응
+		backtracking(cnt + 1);
+
+		//반납
+		alphabet_num[alphabet[cnt]] = -1;
+		num[j] = false;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	for (int i = 0; i < 3; i++) {
+		cin >> word[i];
+	}
+
+	//연산
+	alphabet_cnt = countAlphabet();
+
+	//알파벳의 종류가 10개가 넘는다면 바꿀 수 없으므로 NO 출력
+	if (alphabet_cnt > 10) {
+		cout << "NO";
+		return 0;
+	}
+
+	backtracking(0);
+
+	//출력
+	if (is_possible) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+
+	return 0;
+}
---
+++ Lecture08/AddProblem/BOJ_1759.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int l, c;
+vector<char> alphabet;
+vector<char> password;
+vector<char> vowel = { 'a','e','i','o','u' }; //모음
+vector<char> consonant = { 'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z' }; //자음
+
+//최소 한 개의 모음, 최소 두 개의 자음으로 구성되어 있는지 확인
+bool check() {
+	int vowel_cnt = 0;
+	for (int i = 0; i < vowel.size(); i++) {
+		if (find(password.begin(), password.end(), vowel[i]) != password.end()) {
+			vowel_cnt++;
+		}
+	}
+	if (vowel_cnt < 1) {
+		return false;
+	}
+	
+	int consonant_cnt = 0;
+	for (int i = 0; i < consonant.size(); i++) {
+		if (find(password.begin(), password.end(), consonant[i]) != password.end()) {
+			consonant_cnt++;
+		}
+	}
+	if (consonant_cnt < 2) {
+		return false;
+	}
+
+	return true;
+}
+
+//암호를 출력하는 함수
+void printPassword() {
+	for (auto iter : password) {
+		cout << iter;
+	}
+	cout << "\n";
+}
+
+//cnt: 현재 암호를 이루는 알파벳의 개수, idx: 다음 알파벳의 인덱스
+void backtracking(int cnt, int idx) {
+	//종료 조건: 암호가 다 만들어진 경우
+	if (cnt == l) {
+		//조건에 맞다면 암호 출력
+		if (check()) {
+			printPassword();
+		}
+		return;
+	}
+
+	for (int i = idx; i < c; i++) {
+		//사용
+		password[cnt] = alphabet[i];
+
+		//다음 알파벳 구하기
+		backtracking(cnt + 1, i + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	cin >> l >> c;
+
+	alphabet.assign(c, '0');
+	for (int i = 0; i < c; i++) {
+		cin >> alphabet[i];
+	}
+
+	//알파벳을 순서대로 정렬
+	sort(alphabet.begin(), alphabet.end()); 
+
+	//연산 & 출력
+	password.assign(l, '0');
+	backtracking(0, 0);
+
+	return 0;
+}
