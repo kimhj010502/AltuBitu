d07faed5d02c5ba06271d0c379f14f63bea32059
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-05-04 23:49:59 +0900

    Add files via upload

---
+++ Lecture09/AddProblem/BOJ_9095.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> initDp() {
+	vector<int> dp(12);
+
+	dp[1] = 1; //1
+	dp[2] = 2; //1+1, 2
+	dp[3] = 4; //1+1+1, 1+2, 2+1, 3
+
+	//i = (i-3)+3, (i-2)+2, (i-1)+1
+	for (int i = 4; i <= 11; i++) {
+		dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+	}
+
+	return dp;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int t, n;
+	cin >> t;
+
+	//연산
+	vector<int> dp = initDp();
+
+	while (t--) {
+		//입력
+		cin >> n;
+
+		//출력
+		cout << dp[n] << "\n";
+	}
+
+	return 0;
+}
--- Lecture09/AddProblem/BOJ_9655.cpp
+++ Lecture09/AddProblem/BOJ_9655.cpp
@@ -1,1 +1,26 @@
+﻿#include <iostream>
 
+using namespace std;
+
+string game(int n) {
+	//n이 홀수인 경우 상근이 승리
+	if (n % 2 == 1) {
+		return "SK";
+	}
+	//n이 짝수인 경우 창영이 승리
+	return "CY";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	//연산 & 출력
+	cout << game(n);
+
+	return 0;
+}
