833bc399cfba4af5fcd80fd54c5d0009002559cf
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-05-06 02:04:34 +0900

    Add files via upload

---
+++ Lecture09/AddProblem/BOJ_2156.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int getMax(int n, vector<int>& wine) {
+	//예외 처리
+	if (n == 1) {
+		return wine[0];
+	}
+	if (n == 2) {
+		return wine[0] + wine[1];
+	}
+	if (n == 3) {
+		return max({ wine[0] + wine[1], wine[1] + wine[2], wine[0] + wine[2] });
+	}
+
+	//dp[i]: i번째 포도주까지 마실 수 있는 최대의 양
+	vector<int> dp(n);
+
+	dp[0] = wine[0];
+	dp[1] = wine[0] + wine[1];
+	dp[2] = max({ wine[0] + wine[1], wine[1] + wine[2], wine[0] + wine[2] });
+
+	for (int i = 3; i < n; i++) {
+		dp[i] = max({ dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i], dp[i - 1] });
+	}
+
+	//최댓값 반환
+	int result = 0;
+	for (int i = 0; i < n; i++) {
+		result = max(result, dp[i]);
+	}
+	return result;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	vector<int> wine(n);
+	for (int i = 0; i < n; i++) {
+		cin >> wine[i];
+	}
+
+	//연산 & 출력
+	cout << getMax(n, wine);
+
+	return 0;
+}
---
+++ Lecture09/AddProblem/BOJ_9251.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lcsLength(string s1, string s2) {
+	int n1 = s1.length();
+	int n2 = s2.length();
+
+	vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+	for (int i = 1; i <= n1; i++) {
+		for (int j = 1; j <= n2; j++) {
+			if (s1[i - 1] == s2[j - 1]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+	return dp[n1][n2];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	//연산 & 출력
+	cout << lcsLength(s1, s2);
+
+	return 0;
+}
