1918ec4b247f98668fea0324e057ced4333d131f
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-05-21 02:34:35 +0900

    Add files via upload

---
+++ Lecture12/LiveCoding/BOJ_1991.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+map<char, pair<char, char>> tree;
+
+//전위 순회: root -> left -> right
+void preOrder(char root) {
+	if (root == '.') {
+		return;
+	}
+
+	cout << root;
+	preOrder(tree[root].first);
+	preOrder(tree[root].second);
+}
+
+//중위 순회: left -> root -> right
+void inOrder(char root) {
+	if (root == '.') {
+		return;
+	}
+
+	inOrder(tree[root].first);
+	cout << root;
+	inOrder(tree[root].second);
+}
+
+//후위 순회: left -> right -> root
+void postOrder(char root) {
+	if (root == '.') {
+		return;
+	}
+
+	postOrder(tree[root].first);
+	postOrder(tree[root].second);
+	cout << root;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//입력
+	int n;
+	cin >> n;
+
+	char root, left, right;
+	for (int i = 0; i < n; i++) {
+		cin >> root >> left >> right;
+		tree[root] = { left, right };
+	}
+
+	//연산 & 출력
+	//전위 순회
+	preOrder('A');
+	cout << "\n";
+
+	//중위 순회
+	inOrder('A');
+	cout << "\n";
+
+	//후위 순회
+	postOrder('A');
+
+	return 0;
+}
--- Lecture12/LiveCoding/BOJ_4803.cpp
+++ Lecture12/LiveCoding/BOJ_4803.cpp
@@ -1,1 +1,76 @@
+﻿#include <iostream>
+#include <vector>
 
+using namespace std;
+
+void printResult(int t, int cnt) {
+	cout << "Case " << t << ": ";
+	if (cnt == 0) {
+		cout << "No trees.\n";
+		return;
+	}
+	if (cnt == 1) {
+		cout << "There is one tree.\n";
+		return;
+	}
+	cout << "A forest of " << cnt << " trees.\n";
+}
+
+void dfs(bool& flag, int cur, int prev, vector<vector<int>>& graph, vector<bool>& visited) {
+	//이미 방문했던 곳을 또 방문하는 경우에는 트리가 아님
+	if (visited[cur]) {
+		flag = false;
+		return;
+	}
+
+	visited[cur] = true;
+
+	for (int next : graph[cur]) {
+		//양방향으로 겹치는거 제외
+		if (next == prev) {
+			continue;
+		}
+		dfs(flag, next, cur, graph, visited);
+	}
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m, node1, node2, cnt;
+
+	for (int t = 1;; t++) {
+		//입력
+		cin >> n >> m;
+
+		//n과 m이 둘 다 0인 경우 반복문 종료
+		if (!n && !m) {
+			break;
+		}
+
+		vector<vector<int>> graph(n + 1, vector<int>());
+		while (m--) {
+			cin >> node1 >> node2;
+			graph[node1].push_back(node2);
+			graph[node2].push_back(node1);
+		}
+
+		//연산
+		cnt = 0;
+		vector<bool> visited(n + 1, false);
+		for (int i = 1; i <= n; i++) {
+			bool flag = true; //트리인지의 여부 저장하는 함수
+			dfs(flag, i, 0, graph, visited);
+			if (flag) {
+				cnt++;
+			}
+		}
+
+		//출력
+		printResult(t, cnt);
+	}
+
+	return 0;
+}
