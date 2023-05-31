45f3ff53193fe5861c48d9bf8945f07994f383da
kimhj010502 <101398940+kimhj010502@users.noreply.github.com>
2023-05-31 23:59:10 +0900

    Add files via upload

---
+++ Lecture13/Assignment/BOJ_1865.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+typedef tuple<int, int, int> tp;
+
+bool bellmanFord(int n, vector<tp>& edges) {
+	vector<int> dist(n + 1, 0);
+
+	//노드 하나씩 
+	for (int i = 1; i <= n; i++) {
+		bool flag = false; //음의 사이클 발생 여부
+
+		for (int j = 0; j < edges.size(); j++) {
+			int s = get<0>(edges[j]);
+			int d = get<1>(edges[j]);
+			int w = get<2>(edges[j]);
+
+			int next_weight = dist[s] + w;
+			if (next_weight < dist[d]) {
+				flag = true;
+				dist[d] = next_weight;
+
+				//n번째에 갱신이 되면 음의 사이클이 발생한 것이므로 true 반환
+				if (i == n) {
+					return true;
+				}
+			}
+		}
+		//줄어드는 시간이 없어서 한번도 갱신이 되지 못한 경우 false 반환
+		if (!flag) {
+			break;
+		}
+	}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int tc, n, m, w, s, e, t;
+	cin >> tc;
+
+	while (tc--) {
+		//입력
+		cin >> n >> m >> w;
+
+		vector<tp> edges;
+
+		//양방향
+		while (m--) {
+			cin >> s >> e >> t;
+			edges.push_back({ s,e,t });
+			edges.push_back({ e,s,t });
+		}
+
+		//단방향
+		while (w--) {
+			cin >> s >> e >> t;
+			edges.push_back({ s,e,-t });
+		}
+
+		//연산 & 출력
+		if (bellmanFord(n, edges)) {
+			cout << "YES\n";
+		}
+		else {
+			cout << "NO\n";
+		}
+	}
+
+	return 0;
+}
---
+++ Lecture13/Assignment/PRO_합승택시요금.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int INF = 2e7;
+
+vector<vector<int>> fillGraph(int n, vector<vector<int>>& fares) {
+    vector<vector<int>> graph(n + 1, vector<int>(n + 1, INF));
+    for (int i = 1; i <= n; i++) {
+        graph[i][i] = 0;
+    }
+
+    for (int i = 0; i < fares.size(); i++) {
+        graph[fares[i][0]][fares[i][1]] = fares[i][2];
+        graph[fares[i][1]][fares[i][0]] = fares[i][2];
+    }
+    return graph;
+}
+
+void floyWarshall(int n, vector<vector<int>>& graph) {
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+                int dist = graph[i][k] + graph[k][j];
+                graph[i][j] = min(graph[i][j], dist);
+			}
+		}
+	}
+}
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = INF;
+
+    vector<vector<int>> graph = fillGraph(n, fares);
+    floyWarshall(n, graph);
+
+    for (int i = 1; i <= n; i++) { //i번째 지점까지 합승
+        //지금까지의 최단거리와 합승을 했을 때의 최단거리 비교
+        //이동이 불가능한 경우는 INF이므로 min 조건에 의해 자동으로 고려되지 않음
+        answer = min(answer, graph[s][i] + graph[i][a] + graph[i][b]);
+    }
+    return answer;
+}
+
+int main() {
+    //입력
+    int n = 6, s = 4, a = 6, b = 2;
+    vector<vector<int>> fares = { {4,1,10}, {3,5,24}, {5,6,2}, {3,1,41}, {5,1,24}, {4,6,50}, {2,4,66}, {2,3,22}, {1,6,25} };
+
+    //연산 & 출력
+    cout << solution(n, s, a, b, fares);
+}
